292dcaae903fe898af9acce13a0e08f5095e87ff
OXullo Intersecans <x@brainrapers.org>
2014-01-06 00:02:35 +0100

    Added early implementation of the git handling procedures, using config.Config()

--- gitpullerd/app.py
+++ gitpullerd/app.py
@@ -1,25 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import logging
+
+import git
+
 import server
+import config
 
 logger = logging.getLogger(__name__)
 
 
 class App(object):
-    def __init__(self):
-        self.__server = server.create_server()
+    def __init__(self, ):
+        try:
+            self.__repo = git.Repo(cfg['target_path'])
+        except git.exc.NoSuchPathError:
+            logger.warning('Target path not a git repo, cloning')
+            self.__repo = git.Repo.clone_from(cfg['source_url'], cfg['target_path'])
+
+        self.__repo.git.checkout(cfg['target_branch'])
+
+        self.__server = server.create_server(cfg['webhook_listen_ip'],
+                int(cfg['webhook_listen_port']))
         server.set_payload_handler(self.__payload_handler)
 
     def run(self):
-        self.__server.serve_forever()
+        try:
+            self.__server.serve_forever()
+        except KeyboardInterrupt:
+            logger.info('Terminating')
 
     def __payload_handler(self, payload):
         print 'PAYLOAD:', payload
+        self.__repo.git.pull()
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
+    cfg = config.Config()
+    cfg.from_file('../tests/config.ini')
     app = App()
     app.run()
