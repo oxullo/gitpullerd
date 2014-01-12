47370f1919adec3b8e32fbe4ab31ed0e1cb9eb12
OXullo Intersecans <x@brainrapers.org>
2014-01-12 01:32:25 +0100

    Bark if no config file is found attempting to load one

--- gitpullerd/config.py
+++ gitpullerd/config.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import ConfigParser
+
+class InvalidConfig(Exception):
+    pass
+
 
 class Config(dict):
     def __init__(self):
         super(Config, self).__init__()
 
     def from_file(self, file_name):
+        if not os.path.isfile(file_name):
+            raise InvalidConfig('File %s not found' % file_name)
+
         conf = ConfigParser.ConfigParser()
         conf.read(file_name)
         for section in conf.sections():
